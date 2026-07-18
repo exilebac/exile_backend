80c29941f628b6466fbb46c71c41655813ae3889
Nazboy10 <jorensnazaire10@gmail.com>
2026-07-18 00:50:34 -0400

    correction swager

--- EXILE_B/urls.py
+++ EXILE_B/urls.py
@@ -3,28 +3,28 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from rest_framework.permissions import IsAdminUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('API.urls')),
 
-    # OpenAPI schema
+    # OpenAPI schema toujou disponib (si ou vle)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
-    # Swagger UI (admin only)
-    path(
-        'api/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[IsAdminUser]),
-        name='swagger-ui'
-    ),
-
-    # Redoc UI (admin only)
-    path(
-        'api/schema/redoc/',
-        SpectacularRedocView.as_view(url_name='schema', permission_classes=[IsAdminUser]),
-        name='redoc'
-    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path(
+            'api/schema/swagger-ui/',
+            SpectacularSwaggerView.as_view(url_name='schema'),
+            name='swagger-ui'
+        ),
+        path(
+            'api/schema/redoc/',
+            SpectacularRedocView.as_view(url_name='schema'),
+            name='redoc'
+        ),
+    ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
