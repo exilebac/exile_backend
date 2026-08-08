81c8775dd64057801e36c1b063e6fbcf545d197d
nazboy10 <jorensnazaire10@gmail.com>
2026-08-08 12:33:27 -0400

    Add search filter to VideoViewSet for title and description

--- accueil/views.py
+++ accueil/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Video
 from .serializers import VideoSerializer
 
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.filter(is_public=True).order_by('-created_at')
     serializer_class = VideoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
