fdc4e9b6e4fe45411be0ed49821d9097e3bc43e4
Nazboy10 <jorensnazaire10@gmail.com>
2026-08-10 11:52:42 -0400

    braket konfime

--- API/services/supabase_service.py
+++ API/services/supabase_service.py
@@ -1,11 +1,24 @@
 from supabase import create_client
 from django.conf import settings
 
+# Konekte ak Supabase
 supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
 def upload_video(file, filename):
-    supabase.storage.from_("EXILE_VIDEOS").upload(filename, file)
-    return filename
+    """
+    Upload yon videyo nan bucket EXILE_VIDEOS.
+    Presize MIME type pou evite erè 415 (invalid_mime_type).
+    """
+    res = supabase.storage.from_("Exile_videos").upload(
+        filename,
+        file,
+        {"content-type": "video/mp4"}  # 👈 presize tip fichye a
+    )
+    return res  # retounen repons upload la pou debugging
 
 def get_signed_url(filename, expire=3600):
-    return supabase.storage.from_("EXILE_VIDEOS").create_signed_url(filename, expire)
+    """
+    Kreye yon signed URL pou videyo a.
+    expire default = 3600 segonn (1 èdtan).
+    """
+    return supabase.storage.from_("Exile_videos").create_signed_url(filename, expire)
