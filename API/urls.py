c8e013beca313dc81f7d5bd683b249eb709e4a5d
exilebac <regextrump@gmail.com>
2026-08-21 07:51:44 -0400

    Fix demandes API URL from /demande/demandes/ to /demandes/
    This fixes the 404 error when users try to contact each other.
    The frontend calls /api/v1/demandes/ but the backend was serving
    /api/v1/demande/demandes/ which caused HTML 404 responses instead of JSON.

--- API/urls.py
+++ API/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
+from demande.views import DemandeViewSet
+from rest_framework.routers import DefaultRouter
 
+# Créer un router pour les demandes avec l'URL correcte
+demande_router = DefaultRouter()
+demande_router.register(r'', DemandeViewSet, basename='demande')
 
 urlpatterns = [
     
     path('users/', include('users.urls')),
     path('accueil/', include('accueil.urls')),
-    path('demande/', include('demande.urls')),
+    path('demandes/', include(demande_router.urls)),
     path('evenement/', include('evenement.urls')),
     path('abonnement/', include('abonnement.urls')),
     path('profil/', include('profil.urls')),
